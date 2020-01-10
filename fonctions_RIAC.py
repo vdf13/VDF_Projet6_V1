a08923e6b56801cced1e5b960e74152839727c39
vdf13 <vdf13@free.fr>
2020-01-10 22:26:36 +0100

    Codé exceptions dans fichier fonctions_RIAC.py

--- fonctions_RIAC.py
+++ fonctions_RIAC.py
@@ -2,30 +2,36 @@
 
 import os
 
-# Se positionner sur le répertoire
-os.chdir("c:/01_DATA/PYTHON/VDF_P6")  
-curent_dir = os.getcwd()
+# Se positionner sur le répertoire ou se trouvent les fichiers yaml
+try:
+	os.chdir("c:/01_DATA/PYTHON/VDF_P6")  
+	curent_dir = os.getcwd()
+except FileNotFoundError:
+	curent_dir = os.getcwd()
+	print ("stop")
 
-# lister les fichiers du répertoire
-list_of_files = os.listdir(curent_dir)
-print(list_of_files)
 
-# Trouver un fichier terminant par yml 
-for file in os.listdir(curent_dir):
-    if file.endswith(".yml"):
-        file_to_open = (os.path.join(curent_dir, file))
-        print(file_to_open)
-        # Ouvrir le fichier se terminant par yml
-        file_opened = open(file_to_open, "r")
-        contenu = file_opened.read()
-        print(contenu)
-        file_opened.close()
-   
+
+# Trouver un fichier terminant par yml et l'ouvrir
+try:
+	for file in os.listdir(curent_dir):
+		if file.endswith(".yml"):
+			print(os.path.join(curent_dir, file))
+			# Ouvrir le fichier se terminant par yml
+			file_opened = open(os.path.join(curent_dir, file), "r")
+			contenu = file_opened.read()
+			print(contenu)
+			file_opened.close()
+except UnicodeDecodeError:
+	print("Problème d'ouverture du fichier : {} ".format(file))
+	file_opened.close()
+
 	
 
 # Tester si le fichier txt ou yml est présent 
 
 # Gérer les erreurs d'ouverture de fichier
+# Si le répertoire n'est pas correct : FileNotFoundError: [WinError 2]
 
 
 
