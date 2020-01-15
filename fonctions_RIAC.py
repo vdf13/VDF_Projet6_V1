0bc568fefaf06d5ecb3c9d659fb1110411285288
Victor <vdf13@free.fr>
2020-01-15 10:24:55 +0100

    modif fichier fonction RIAC

--- fonctions_RIAC.py
+++ fonctions_RIAC.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+""" Fontion destiné à l'ouverture de fichier yml 
+
+- se positionne dans le répertoire défini
+- ouvre les fichiers yml présent
+- gère les erreurs de répertoire non disponible
+- gère les erreurs de fichiers corrompus
+
+"""
 
 import os
 
@@ -12,26 +20,22 @@ except FileNotFoundError:
 
 
 
-# Trouver un fichier terminant par yml et l'ouvrir
+# Ouvre les fichiers YAML (avec l'extension yml)
 try:
 	for file in os.listdir(curent_dir):
 		if file.endswith(".yml"):
-			print(os.path.join(curent_dir, file))
+			#print(os.path.join(curent_dir, file))
+			print("Fichier en cours de traitement : {} ".format(file))
 			# Ouvrir le fichier se terminant par yml
 			file_opened = open(os.path.join(curent_dir, file), "r")
-			contenu = file_opened.read()
+			contenu = file_opened.read() # Passer le contenu a une variable
 			print(contenu)
 			file_opened.close()
 except UnicodeDecodeError:
-	print("Problème d'ouverture du fichier : {} ".format(file))
+	print("Problème lors de l'ouverture du fichier : {} ".format(file))
 	file_opened.close()
 
 	
-
-# Tester si le fichier txt ou yml est présent 
-
-# Gérer les erreurs d'ouverture de fichier
-# Si le répertoire n'est pas correct : FileNotFoundError: [WinError 2]
 
 
 
