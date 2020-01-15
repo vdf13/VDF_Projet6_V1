d8c238d5bfe12794f54f5f11e873e72609248f3e
Victor <vdf13@free.fr>
2020-01-15 14:16:47 +0100

    Définitions des principales méthodes du programme

--- fonctions_RIAC.py
+++ fonctions_RIAC.py
@@ -31,6 +31,48 @@ def ouverture(monfichier):
 	except:
 		print("Autre erreur")
 
+def installation():
+	""" Le but de cette methode est d'installer un role sur un serveur
+	
+	- Détecte le nom du rôle et IP du serveur dans le fichier YAML
+	- Se connecte sur le serveur en ssh avec compte admin
+	- Installe le role suivant commandes du template
+
+	"""
+
+def configuration():
+	""" Le but de cette méthode est de configurer le rôle sur un serveur suite à l'installation.
+	
+	- Détecte le nom du rôle et IP du serveur dans le fichier YAML
+	- Se connecte sur le serveur en ssh avec compte admin
+	- Configure le serveur en fonction des éléments récupéré du fichier YAML
+
+	"""
+
+	# ouvrir le fichier et récupérer dans un dictionnaire les données
+	contenu = ouverture(monfichier)
+	
+
+
+def tests():
+	""" Le but de cette méthode est de tester que le serveur installé et configuré est opérationnel.
+
+	- Détecte le nom du rôle et IP du serveur dans le fichier YAML
+	- Se connecte sur le serveur en ssh avec compte admin 
+	- Lance les tests en fonction des éléments présent dans le fichier YAML
+
+	"""
+
+
+def connexion_IP():
+	""" Le but de cette méthode est d'établir une connexion ssh avec le serveur
+
+	- Détecte le nom du rôle et IP du serveur dans le fichier YAML
+	- Se connecte sur le serveur en ssh avec compte admin 
+
+	"""
+
+
 
 
 
