bea891e95caa5b6c8ed7d3e20e079c914091716c
Victor <vdf13@free.fr>
2020-01-18 16:14:58 +0100

    fin du code de la classe DhcpInterface

--- test_class.py
+++ test_class.py
@@ -7,21 +7,29 @@ class DhcpInterface:
 	""" Classe qui va créer l'objet fichier isc-dhcp-server
 	Attributs de cette classe :
 	- nom du fichier qui sera créé
-	- valeur carte interface dhcp V4
-	- valeur carte interface dhcp V6
+	- valeur de la clé cherché dans le dictionnaire
+	- valeur 
 
 	"""
-	def __init__(self, interfaceV4='', interfaceV6='', text_to_write=''):
+	def __init__(self, input_yml={}):
 		""" Création des attributs de l'objet fichier isc-dhcp-server """
 		self.file_name = "C:\\01_DATA\\PYTHON\\VDF_P6\\isc-dhcp-server"
-		self.interfaceV4 = interfaceV4
-		self.interfaceV6 = interfaceV6
-		self.text_to_write = text_to_write
+		self.key_interfaceV4 = 'dhcp_interface'
+		self.key_interfaceV6 = 'dhcp_interface'
+		self.text_interfaceV4 = 'INTERFACES="' + input_yml[self.key_interfaceV4] + '"'
+		self.text_to_write = ''
+		self.input_yml = input_yml
+
+		if self.key_interfaceV4 in self.input_yml:
+			self.write(self.text_interfaceV4)
+			
 	def write(self, text_to_add):
 		""" Méthode pour écrire le texte dans le fichier """
 		if self.text_to_write != "":
 			self.text_to_write += "\n"
 		self.text_to_write += text_to_add
+		return self.text_to_write
+
 
 class DictInputYaml(dict):
 	""" Classe qui va créer l'objet dictionnaire avec les clés et valeurs du fichier yaml
@@ -33,11 +41,18 @@ class DictInputYaml(dict):
 	def __init__(self, base={},):
 		self._cles = []
 		self._valeurs = []
+		return 
 
 	# trouver moyen de convertir du teste str en valeur pour dict
 
-# fonction nettoyage de texte
+# fonction nettoyage de texte et renvoie un dictionnaire
 def cleaning_yml(data):
+	""" Fonction de traitement du texte du fichier yaml
+	- enlève les "---" du début du fichier
+	- enlève les retours lignes "\n"
+	- enlève les "- " du début de ligne
+	- sépare la ligne sur le signe ":" et renvoie un dictionnaire
+	"""
 	cleaning = []
 	cleaned = {}
 	for texte in data:
@@ -63,24 +78,37 @@ def ouverture(monfichier):
 	except:
 		print("Autre erreur")
 
+# Partie pour écrire le résultat dans un fichier
+def ecrire_fichier(monfichier, contenu):
+	try:
+		with open(monfichier, "w") as file_to_close:
+			file_to_close.write(contenu)
+	except:
+		print("Erreur d'écriture du fichier {} ".format(monfichier))
+		file_to_close.close()
 
 
-# partie test objet DhcpIterface
-file = DhcpInterface('eth0', )
-print(file.file_name)
-print(file.interfaceV4)
-print(file.text_to_write)
 
 # partie test objet DictInputYaml
 file_2 = "C:\\01_DATA\\PYTHON\\VDF_P6\\test_data_class"
 contenu = ouverture(file_2)
-print("avant formatage \n",contenu)
+#print("avant formatage \n",contenu)
 # test enlever du texte
 final = cleaning_yml(contenu)
-print("après formatage \n",final)	
+#print("après formatage , le dictionnaire créé \n",final)	
 
+# partie test objet DhcpInterface
+file = DhcpInterface(input_yml = final)
+print(file.file_name)
+#print(file.key_interfaceV4)
+#print(file.text_interfaceV4)
+print(file.text_to_write)
 
-entree = DictInputYaml()
-print("\n contenu du dict entree :",entree)
+ecrire_fichier(file.file_name, file.text_to_write)
+
+# essais de création d'un dictionnaire, ne semble plu utile avec la fonction cleaning
+
+#entree = DictInputYaml()
+#print("\n contenu du dict entree :",entree)
 
 
