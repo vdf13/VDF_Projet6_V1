74b0d35af112e2d0c613d9cd78ee6b8d8510ecdf
Victor <vdf13@free.fr>
2020-01-20 16:15:48 +0100

    fin des tests sur modele_class.py

--- modele_class.py
+++ modele_class.py
@@ -7,17 +7,20 @@ class Da:
 	""" Classe qui va créer l'objet fichier modifiable
 	Attributs de cette classe :
 	- nom fichier, carac, dico defaut
-	- méthode , se créer, lire, modifier, écrire texte
+	- méthode , se créer, lire, modifier défaut, écrire texte, substituer texte
 	
 	"""
 	def __init__(self, entree={}, text_to_write=''):
 		""" Création des attributs de l'objet  """
-		self.file_name = "C:\\01_DATA\\PYTHON\\VDF_P6\\Da"
-		self.def_dict = { "a": "A", "b": "B", "d": "D", "e": "E"}
+		self.file_name = "C:\\01_DATA\\PYTHON\\VDF_P6\\dhcpd.conf"
+		self.input_file = "C:\\01_DATA\\PYTHON\\VDF_P6\\dhcpd.conf_template_debian"
+		self.def_dict = { "option domain-name ": "AAAAAA", "option domain-name-servers": "BBBBBBB, 222222", "default-lease-time": "DDDDDDDD"}
 		self.text_to_write = text_to_write
+		self.lines = ouverture(self.input_file)
 		self.entree = entree
 		# liste des clés 
-		self.local_keys = ["a", "b", "d", "e"]
+		self.local_keys = ["option domain-name ", "option domain-name-servers", "default-lease-time"]
+		self.end_line = ";\n"
 
 		
 
@@ -42,40 +45,57 @@ class Da:
 
 	def substitue(self):
 		""" Méthode qui va remplacer dans le texte les valeurs modifiés ou défaut"""
-		print("avant", self.text_to_write)
 		texte =''
-		lines = self.text_to_write.splitlines()
+		#lines = self.text_to_write.splitlines()
+		# lines = self.text_to_write
 		#print(lines)
-		for line in lines:
-			if str(line).startswith("#") is False:
-				for val in self.local_keys:
-					if val in line:
-						texte += val + " " + self.def_dict[val] + "\n"
-						
-
-				
-			else:
+		for line in self.lines:
+			if str(line).startswith("#") is True:
+				# cas ou il y a un # en début de ligne, on recopie la ligne
 				texte += line + "\n"
 
+											
+			else:
+				# Cas ou la ligne ne commence pas par #
+				for val in self.local_keys:
+					if val in line:
+						cc = line.find(val, 0, len(line))
+						texte += line[:cc] + val + " " + self.def_dict[val] + self.end_line
+						break # Important pour finir la condition
+				else:
+					texte += line + "\n"
 
+		self.text_to_write = texte
+		ecrire_fichier(self.file_name, self.text_to_write)
 
-		print("après text_to_write :\n", self.text_to_write)
-		print("après texte : \n", texte)
-		self.write
+def ouverture(monfichier):
+	try:
+		with open(monfichier, "r") as file_opened:
+			contenu = file_opened.readlines()
+			#return file_opened.read()
+			return contenu
+	except:
+		print("Autre erreur")
 
-		
-
-
+def ecrire_fichier(monfichier, contenu):
+	try:
+		with open(monfichier, "w") as file_to_close:
+			file_to_close.write(contenu)
+	except:
+		print("Erreur d'écriture du fichier {} ".format(monfichier))
+		file_to_close.close()
 
 
 # début phase de test
 local = {"a": "x", "b": "B", "c": "C", "d": "D", "e": "x", "f": "xx"}
-modele = "#Voici mon texte\n# Diese\n# Diese\na\nb\nTexte c\n# Encore Diese"
+modele = "#Voici mon texte\n# Diese\n# Diese\na\nb\nTexte d\n# Encore Diese"
 tdef = Da()
 tnew = Da(local, modele)
 print("commande print t par défaut  : ", tdef.def_dict)
 print("commande print t new  : ", tnew.def_dict)
+
 tnew.update()
 print("commande print t new  après update : ", tnew.def_dict)
-#print(tnew.text_to_write)
+
 tnew.substitue()
+print("final", tnew.text_to_write)
