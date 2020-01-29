e0b7fd0ff112059a7b32d8423eefe8e91ecaac86
vdf13 <vdf13@free.fr>
2020-01-29 07:20:26 -0500

    ajout des expression régulières dans le code

--- new_fonctions.py
+++ new_fonctions.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python2.7
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 
-import os
+
+import os, yaml, re
+
 
 
 
@@ -81,28 +82,80 @@ class DhcpdConf:
 		""" Création des attributs de l'objet  """
 		self.output_file = "/home/administrateur/VDF_P6/dhcpd.conf"
 		self.input_file = "/home/administrateur/VDF_P6/dhcpd.conf_template_debian"
-		self.default_dict = { "option domain-name ": "exemple.org", \
-		 "option domain-name-servers": "8.8.8.8, 8.8.4.4", \
+		self.default_dict = { "domain-name": "exemple.org", \
+		 "domain-name-servers": "8.8.8.8, 8.8.4.4", \
 		  "default-lease-time": "600", \
 		  "max-lease-time": "7200", \
-		  "authoritative": "#authoritative" \
-		  }
-		self.text_to_write = text_to_write
-		self.lines = ouverture(self.input_file)
-		self.entree = entree
-		# liste des clés 
-		self.local_keys = ["option domain-name ", \
-		 "option domain-name-servers", \
+		  "routers": "routeur1.exemple.org, routeur2.exemple.org", \
+		  "authoritative": "#authoritative", \
+		  "subnet": "172.16.100.0", \
+  		  "range": "172.16.100.10 172.16.100.20" \
+  		  }
+		 # enlever cette partie , ne sers plus !
+		'''
+		self.local_keys = ["domain-name", \
+		 "domain-name-servers", \
 		 "default-lease-time", \
 		 "max-lease-time", \
 		 "authoritative" \
-		 ]
+		 ]  
+		'''
+		self.text_to_write = text_to_write
+		self.lines = ouverture(self.input_file)
+		# pour utiliser le module re texte chargé en intégralité
+		self.lines_re = ouverture_re(self.input_file)
+		# c'est le dictionnaire Vars qui est chargé en entree
+		
+		self.entree = entree
+		
+		# symbole a ajouter pour finir la ligne
 		self.end_line = ";\n"
 
-		for key in self.local_keys:
-			if key in self.entree:
-				self.default_dict[key] = self.entree[key]
-		self.substitue()
+
+		
+		# on remplace le dictionnaire par défaut par les valeurs chargées //// Poursuivre
+		for key in self.entree.keys():
+			self.default_dict[key] = entree[key]
+		print("default dict après = ", self.default_dict)
+
+		# on lance le modue qui utilise re
+		self.resub()
+			
+		# on apelle la fonction de remplacement du texte
+		#self.substitue()
+	# création auutre méthode substitue avec utilisation des expressions régulières
+	def resub(self):
+		''' 
+		méthode pour substituer le texte par les valeurs du dictionnaire modifié
+		utilse le module re pour les expressions régulières
+
+		'''
+		texte = ' \nUne phrase avec domain-name et range qui.peut.se.trouver et netmask dans; fichier source \n\
+option domain-name "example.org";\n\
+option domain-name-servers ns1.example.org, ns2.example.org;\n\
+default-lease-time 600;\n\
+max-lease-time 7200;\n\
+subnet 10.254.239.0 netmask 255.255.255.224 {\n\
+  range 10.254.239.10 10.254.239.20;\n\
+  option routers rtr-239-0-1.example.org, rtr-239-0-2.example.org;\
+}\n'
+
+		print(texte)
+
+
+		exp = "([ ]+\w*)"	# fonctionne pour un espace + mot sans symbole
+		exp1 = '([ ]+[\w".,]*)'	# fonctionne pour espace + mot avec alpha + " . enlève tout jusque la , ;
+		
+		for key in self.default_dict:
+			#print(key)
+			#if key in self.lines_re:
+			if key in texte:
+				#re.sub(r"{0}".format(key), "toto", self.lines_re)
+				texte = re.sub(r"{0}{1}*".format(key, exp1), "{0} {1}".format(key, self.default_dict[key]), texte)
+				#print("Trouvé ")
+				
+
+		print(texte)
 	
 	def write(self, text_to_add=''):
 		""" Méthode pour écrire le texte dans le fichier """
@@ -123,10 +176,10 @@ class DhcpdConf:
 				texte += line + "\n"								
 			else:
 				# Cas ou la ligne ne commence pas par #
-				for val in self.local_keys:
+				for val in list(self.default_dict):
 					if val in line:
 						cc = line.find(val, 0, len(line))
-						texte += line[:cc] + val + " " + self.default_dict[val] + self.end_line
+						texte += line[:cc] + str(val) + " " + str(self.default_dict[val])+ self.end_line
 						break # Important pour finir la condition
 				else:
 					texte += line + "\n"
@@ -143,9 +196,18 @@ class DhcpdConf:
 
 # Définir la fonction ouverture du fichier yml ligne par ligne pour envoie à la fonction cleaning_yml
 def ouverture_yml(monfichier):
+	with open(monfichier, "r") as file_opened:
+		try:
+			return yaml.safe_load(file_opened)
+		except yaml.YAMLError as exc:
+			print(exc)
+
+def ouverture_re(monfichier):
+	""" ouverture des fichiers en mode ligne par ligne """
+	# voir si pas doublon avec le fonction ouverture_yml qui est aussi en mode ligne
 	try:
 		with open(monfichier, "r") as file_opened:
-			contenu = file_opened.readlines()
+			contenu = file_opened.read()
 			return contenu
 	except:
 		print("Autre erreur")
