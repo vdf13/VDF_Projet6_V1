d7627792c4ffcf05ff0d7d784debc780761775ed
vdf13 <vdf13@free.fr>
2020-01-29 07:20:26 -0500

    ajout des expression régulières dans le code

--- new_main.py
+++ new_main.py
@@ -1,34 +1,55 @@
-#!/usr/bin/python2.7
-# -*- coding: utf-8 -*-
+ #-*- coding: utf-8 -*-
 
 
-#import os
+import os, sys, yaml
 from new_fonctions import *
 
 
+
+
+
 # Création d'un dictionnaire à partir d'un fichier yaml exemple
-file_yml = "/home/administrateur/VDF_P6/fichier-entree.yml"
+#file_yml = "/home/administrateur/VDF_P6/fichier-entree.yml"
 
 # partie à developper pour trier dans les rôles
+'''
 def choix_role(dictionnaire_yml):
 	if 'role_name' in dictionnaire_yml.keys():
 		role_demande = dictionnaire_yml['role_name']
 		print("rôle trouvé : ", role_demande)
-
+'''
 
 
 # création du dictionnaire avec les valeurs lu du fichier yaml
-val_yml = cleaning_yml(ouverture_yml(file_yml))
-choix_role(val_yml)
+#val_yml = cleaning_yml(ouverture_yml(file_yml))
+#choix_role(val_yml)
 
 
 # création de l'objet dhcpd.conf
-dhcpd = DhcpdConf(val_yml)
-print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dhcpd.output_file, file_yml))
+#dhcpd = DhcpdConf(val_yml)
+#print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dhcpd.output_file, file_yml))
 
 # Test de l'objet dns  named.conf
+'''
+a developper plus tard
 test_dns_yml = "/home/administrateur/VDF_P6/test_dns.yml"
 test_dns = cleaning_yml(ouverture_yml(test_dns_yml))
 dns = Dns(test_dns)
 print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1}".format( dns.output_file, test_dns))
+'''
 
+# variables pour ouvrir le fichier yaml avec les arguments fichier et role recherché
+fichier_yml = sys.argv[1]
+info = sys.argv[2]
+
+# ouverture du fichier source yaml et chargement des parametres dans le dictionnaire Vars
+Vars = ouverture_yml(fichier_yml)
+#print(Vars['role_name'])
+
+# Test du role trouvé dans la variable Vars et parametre lancé a l'execution ex: python.py fichier dhcp
+if Vars['role_name'][info] == 'dhcp':
+	print("1er print", Vars['role_name'][info])
+	dhcpd = DhcpdConf(Vars['role_name'])
+	print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dhcpd.output_file, fichier_yml))
+else:
+	print("pas trouvé de role à installer")
