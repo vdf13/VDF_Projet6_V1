b53acee65ec852c32af8e159827805bfdbfd291a
vdf13 <vdf13@free.fr>
2020-02-01 07:17:15 -0500

    Partie dns presque prête, reste ficfier zone à faire

--- new_main.py
+++ new_main.py
@@ -1,4 +1,5 @@
- #-*- coding: utf-8 -*-
+#!/usr/bin/python2.7
+#-*- coding: utf-8 -*-
 
 
 import os, sys, yaml
@@ -6,10 +7,6 @@ from new_fonctions import *
 
 
 
-
-
-# Création d'un dictionnaire à partir d'un fichier yaml exemple
-#file_yml = "/home/administrateur/VDF_P6/fichier-entree.yml"
 
 # partie à developper pour trier dans les rôles
 '''
@@ -19,28 +16,14 @@ def choix_role(dictionnaire_yml):
 		print("rôle trouvé : ", role_demande)
 '''
 
-
-# création du dictionnaire avec les valeurs lu du fichier yaml
-#val_yml = cleaning_yml(ouverture_yml(file_yml))
-#choix_role(val_yml)
-
-
 # création de l'objet dhcpd.conf
 #dhcpd = DhcpdConf(val_yml)
 #print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dhcpd.output_file, file_yml))
 
-# Test de l'objet dns  named.conf
-'''
-a developper plus tard
-test_dns_yml = "/home/administrateur/VDF_P6/test_dns.yml"
-test_dns = cleaning_yml(ouverture_yml(test_dns_yml))
-dns = Dns(test_dns)
-print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1}".format( dns.output_file, test_dns))
-'''
 
 # variables pour ouvrir le fichier yaml avec les arguments fichier et role recherché
-fichier_yml = sys.argv[1]
-info = sys.argv[2]
+fichier_yml = sys.argv[1]	# nom du fichier en premier parametre
+info = sys.argv[2]			# title pour lire le nom du role à configurer
 
 # ouverture du fichier source yaml et chargement des parametres dans le dictionnaire Vars
 Vars = ouverture_yml(fichier_yml)
@@ -54,6 +37,10 @@ if Vars['role_name'][info] == 'dhcp':
 	print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dhcpd.output_file, fichier_yml))
 	isc = IscDhcpServer(Vars['role_name'])
 	print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( isc.output_file, fichier_yml))
+elif Vars['role_name'][info] == 'dns':
+	print("Le rôle {0} va être configuré sur le serveur : {1}\n".format(Vars['role_name'][info], IP))
+	dns = Dns(Vars['role_name'])
+	print("Le fichier {0} viens d'être créé avec les valeurs du fichier {1} .".format( dns.output_file, fichier_yml))
 else:
 	print("pas trouvé de role à installer")
 
